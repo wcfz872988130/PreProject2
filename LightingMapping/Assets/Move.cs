c5304b9320616f96ab03f0853b9380c0093486b0
NetEase <netease@NetEasedeiMac-10.local>
2016-08-08 21:24:49 +0800

    LightingMapping

---
+++ LightingMapping/Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			gameObject.transform.Translate (new Vector3(Time.deltaTime*5,0,0));
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			gameObject.transform.Translate (new Vector3(-Time.deltaTime*5,0,0));
+		}
+	}
+}
